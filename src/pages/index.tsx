7bc069eb262b8861366154a4ae7eeaa8cf7d1e61
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-06-07 21:30:24 -0300

    add content main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
 import Head from "next/head";
+import Image from "next/image";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./main.module.scss";
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1>Hello Word</h1>
+      <main className={styles.mainContainer}>
+        <section className={styles.mainContent}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br /> the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <span>for R$10,50 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/imagens/woman.svg" alt="woman in desk with computer" />
+      </main>
     </>
   );
 }
