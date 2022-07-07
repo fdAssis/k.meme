d603d84d27b198a977e56cb72d236624ea461214
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2022-07-07 17:20:46 -0300

    feat: redirection for payment

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,11 +1,39 @@
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../service/api";
+import { getStripeJs } from "../../service/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  async function handleSubcribe() {
+    if (!session) {
+      signIn("google");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subcribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubcribe}
+    >
       Subscribe now
     </button>
   );
